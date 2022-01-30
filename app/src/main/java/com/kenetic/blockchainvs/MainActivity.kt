a2253daac9cb3ef03f53b2aa41de8aacd8981fb7
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-01-30 16:23:23 +0530

    basic supports added, viewBinding and dataBinding Enabled

--- app/src/main/java/com/kenetic/blockchainvs/MainActivity.kt
+++ app/src/main/java/com/kenetic/blockchainvs/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.kenetic.blockchainvs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.kenetic.blockchainvs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
+                .navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
